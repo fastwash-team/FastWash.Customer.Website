19c702a1498a43592d10fbc2e1bd4c3b33ea05e0
Excellence D. <eoyeniran@tradedepot.co>
2023-12-11 03:06:11 +0100

    chore: worked on the mobile summary div

--- src/pages/schedule-pickup.tsx
+++ src/pages/schedule-pickup.tsx
@@ -112,6 +112,64 @@ export function SchedulePickup() {
                   <b>NGN 5690</b>
                 </div>
               </div>
+              <div className='schedule-pickup__body__steps-summary_mobile'>
+                <div className='accordion' id='accordionExample'>
+                  <div className='accordion-item'>
+                    <h2 className='accordion-header'>
+                      <button
+                        className='accordion-button'
+                        type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#collapseOne'
+                        aria-expanded='false'
+                        aria-controls='collapseOne'
+                      >
+                        Request Summary
+                      </button>
+                    </h2>
+                    <div
+                      id='collapseOne'
+                      className='accordion-collapse collapse'
+                      data-bs-parent='#accordionExample'
+                    >
+                      <div className='accordion-body'>
+                        <div className='address-block'>
+                          <p>Address</p>
+                          <b>No 4, Queen Street, Alagomeji-Yaba, Yaba, Lagos</b>
+                        </div>
+                        <div className='billing-items'>
+                          <div className='item'>
+                            <span>Service Type</span>
+                            <b>
+                              {selectedWashType === PRESCHEDULED_WASH
+                                ? "Pre-Scheduled Wash"
+                                : selectedWashType === CLASSIC_WASH
+                                ? "Classic Wash"
+                                : ""}
+                            </b>
+                          </div>
+                          <div className='item'>
+                            <span>Pick up Time</span>
+                            <b>{pickupRange}</b>
+                          </div>
+                          <div className='item'>
+                            <span>Est Delivery Time</span>
+                            <b>Today</b>
+                          </div>
+                          <div className='item'>
+                            <span>Logistics</span>
+                            <b>NGN 1000</b>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>{" "}
+                <div className='total'>
+                  <span>Total</span>
+                  <b>NGN 5690</b>
+                </div>
+              </div>
             </div>
           </div>
           <button className='mt-4 next-button'>Next</button>
