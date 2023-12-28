5ef72fd23dba17c8de1c1f2ea51b3c7df3f301ef
Excellence D. <eoyeniran@tradedepot.co>
2023-12-28 13:33:27 +0100

    fix: removed unused variable

--- src/components/info-message.tsx
+++ src/components/info-message.tsx
@@ -2,7 +2,7 @@ import { InfoMessageComponentProps } from "../utils/types";
 
 export const InfoMessage = ({
   message,
-  type = "error",
-}: InfoMessageComponentProps) => {
+}: // type = "error",
+InfoMessageComponentProps) => {
   return <p className='error-message-text'>{message}</p>;
 };
